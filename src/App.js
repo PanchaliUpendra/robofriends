96b77e7541c94445583c3760d02c64e45787caeb
PanchaliUpendra <upendrapanchali83@gmail.com>
2022-04-01 16:09:44 +0530

    deploy my robotic app to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React ,{Component} from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+//import {robots} from './robots';
+import Scroll from './Scroll';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+    constructor(){
+        super()
+        this.state ={
+            robots:[],
+            searchfield:''
+        }
+        
+    }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots:users}));
 }
+    
+
+    onSearchChange= (event) => {
+        this.setState({searchfield: event.target.value})
+       }
+
+       render() {
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot =>{
+          return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+        return !robots.length ?
+          <h1>Loading</h1> :
+          (
+            <div className='tc'>
+              <h1 className='f1'>RoboFriends</h1>
+              <SearchBox searchChange={this.onSearchChange}/>
+              <Scroll>
+                <CardList robots={filteredRobots} />
+              </Scroll>
+            </div>
+          );
+      }
+    }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+
+ReactDOM.render(<App/>,
+          
   document.getElementById('root')
 );
 
